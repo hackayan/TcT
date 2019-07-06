f3a18e73b5c8ed2b2d5b5e137780d8f563640979
imflammable <rahulsingh7716@gmail.com>
2019-07-06 10:17:17 +0530

    Initial commit

---
+++ app/src/main/java/com/example/tct/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.tct;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    Button a;
+    TextView b;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
